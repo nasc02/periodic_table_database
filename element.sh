0b501cded533ccbb472b88754611a5d553a59454
nasc02 <henriqueaugusto840@gmail.com>
2023-08-08 15:13:46 +0000

    fix:Second commit

---
+++ element.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [ $# -eq 0 ]; then
+    echo "Please provide an element as an argument."
+    exit 1
+fi
+
+argument="$1"
+
+
+atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = '$argument'")
+if [ -z $atomic_number ]; 
+then
+  atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$argument'")
+   if [[ -z $atomic_number ]]
+   then
+    atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$argument'")
+      if [[ -z $name ]] 
+      then
+        echo "Please provide an element as an argument."
+        exit 1
+      fi
+   fi
+fi
+
+element_name=$($PSQL "SELECT name FROM elements WHERE $atomic_number = atomic_number")
+element_symbol=$($PSQL "SELECT symbol FROM elements WHERE $atomic_number = atomic_number")
+element_type=$($PSQL "SELECT type FROM properties WHERE $atomic_number = atomic_number")
+element_mass=$($PSQL "SELECT atomic_mass FROM properties WHERE $atomic_number = atomic_number")
+element_melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE $atomic_number = atomic_number")
+element_boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE $atomic_number = atomic_number")
+
+echo "The element with atomic number $atomic_number is $element_name($element_symbol). It's a $element_type, with a mass of $element_mass amu. $element_name has a melting point of $element_melting_point celsius and a $element_boiling_point celsius."
