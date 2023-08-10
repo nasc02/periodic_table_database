f64b3776b60f2408ba04091120df59bda0f619f3
nasc02 <henriqueaugusto840@gmail.com>
2023-08-10 14:45:43 +0000

    fix:ninth commit

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 if [ $# -eq 0 ]; then
     echo "Please provide an element as an argument."
-    exit 1
+    exit 0
 fi
 
 argument="$1"
@@ -25,13 +25,15 @@ atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = 
 #    fi
 # fi
 
-type_id=$($PSQl "SELECT type_id FROM properties WHERE atomic_number = $atomic_number")
+# type_id=$($PSQl "SELECT type_id FROM properties WHERE atomic_number = $atomic_number")
+# element_type=$($PSQL "SELECT type FROM types WHERE $type_id = type_id")
 
 element_name=$($PSQL "SELECT name FROM elements WHERE $atomic_number = atomic_number")
 element_symbol=$($PSQL "SELECT symbol FROM elements WHERE $atomic_number = atomic_number")
-element_type=$($PSQL "SELECT type FROM types WHERE $type_id = type_id")
 element_mass=$($PSQL "SELECT atomic_mass FROM properties WHERE $atomic_number = atomic_number")
 element_melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE $atomic_number = atomic_number")
 element_boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE $atomic_number = atomic_number")
+
+element_type=$($PSQL "SELECT t.type FROM properties p JOIN types t ON p.type_id = t.type_id WHERE p.atomic_number = $atomic_number")
 
 echo "The element with atomic number $atomic_number is $element_name($element_symbol). It's a $element_type, with a mass of $element_mass amu. $element_name has a melting point of $element_melting_point celsius and a boiling point $element_boiling_point celsius."
