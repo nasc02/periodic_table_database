57f2791302b7752ef2d89ad120850622b75a525b
nasc02 <henriqueaugusto840@gmail.com>
2023-08-10 15:12:36 +0000

    refactor:rework point commit

--- element.sh
+++ element.sh
@@ -11,29 +11,21 @@ argument="$1"
 
 
 atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number = $(($argument)) OR symbol = '$argument' OR name = '$argument'")
-# if [ -z $atomic_number ]; 
-# then
-#   atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE symbol = '$argument'")
-#    if [[ -z $atomic_number ]]
-#    then
-#     atomic_number=$($PSQL "SELECT atomic_number FROM elements WHERE name = '$argument'")
-#       if [[ -z $atomic_number ]] 
-#       then
-#         echo "Please provide an element as an argument."
-#         exit 1
-#       fi
-#    fi
-# fi
 
-# type_id=$($PSQl "SELECT type_id FROM properties WHERE atomic_number = $atomic_number")
-# element_type=$($PSQL "SELECT type FROM types WHERE $type_id = type_id")
+if [[ -z $atomic_number ]]
+then
+    echo "I could not find that element in the database."
+    exit 0
+else
+    element_name=$($PSQL "SELECT name FROM elements WHERE $atomic_number = atomic_number")
+    element_symbol=$($PSQL "SELECT symbol FROM elements WHERE $atomic_number = atomic_number")
+    element_mass=$($PSQL "SELECT atomic_mass FROM properties WHERE $atomic_number = atomic_number")
+    element_melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE $atomic_number = atomic_number")
+    element_boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE $atomic_number = atomic_number")
 
-element_name=$($PSQL "SELECT name FROM elements WHERE $atomic_number = atomic_number")
-element_symbol=$($PSQL "SELECT symbol FROM elements WHERE $atomic_number = atomic_number")
-element_mass=$($PSQL "SELECT atomic_mass FROM properties WHERE $atomic_number = atomic_number")
-element_melting_point=$($PSQL "SELECT melting_point_celsius FROM properties WHERE $atomic_number = atomic_number")
-element_boiling_point=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE $atomic_number = atomic_number")
+    element_type=$($PSQL "SELECT t.type FROM properties p JOIN types t ON p.type_id = t.type_id WHERE p.atomic_number = $atomic_number")
 
-element_type=$($PSQL "SELECT t.type FROM properties p JOIN types t ON p.type_id = t.type_id WHERE p.atomic_number = $atomic_number")
+    echo "The element with atomic number $atomic_number is $element_name ($element_symbol). It's a $element_type, with a mass of $element_mass amu. $element_name has a melting point of $element_melting_point celsius and a boiling point of $element_boiling_point celsius."
+    exit 0
+fi
 
-echo "The element with atomic number $atomic_number is $element_name($element_symbol). It's a $element_type, with a mass of $element_mass amu. $element_name has a melting point of $element_melting_point celsius and a boiling point $element_boiling_point celsius."
